2fe2aced1116d6bb32fb95f05d9e24a486e40cbc
boevetss <73750523+boevetss@users.noreply.github.com>
2022-05-27 00:14:47 +0300

    обновила файлы, добавила раличные виды сглаживания графиков

--- main.py
+++ main.py
@@ -9,83 +9,132 @@ def main ():
     # обработка данных
         # для примера используется файл с измерениями P1_2 
         table = pd.read_excel('examlpe.xlsx')
+        #table = pd.read_excel('examlpe5.xlsx')
 
         # x
-        time = table.values[:, 0]
+        hz = table.values[:, 1]
 
         # y
-        l_tremor = table.values[:, 1]
-        r_tremor = table.values[:, 2]
-        # l_spectrum = table.values[:, 3]
-        # r_spectrum = table.values[:, 4]
+        l_spectrum = table.values[:, 5]
+        r_spectrum = table.values[:, 9]
 
         # одномерный массив
-        time_new = np.linspace(time.min(), time.max(), 200)
+        hz_new = np.linspace(hz.min(), hz.max(), 200)
 
         # k = 3 - кубический сплайн
-        spl_l = interpolate.make_interp_spline(time, l_tremor, k=3)
-        spl_r = interpolate.make_interp_spline(time, r_tremor, k=3)
-        spl_tremor_l = spl_l(time_new)
-        spl_tremor_r = spl_r(time_new)
+        spl_l = interpolate.make_interp_spline(hz, l_spectrum, k=3)
+        spl_r = interpolate.make_interp_spline(hz, r_spectrum, k=3)
+        spl_spectrum_l = spl_l(hz_new)
+        spl_spectrum_r = spl_r(hz_new)
 
-        # вычисление интеграла
-        area_l = integrate.trapz(spl_tremor_l, x = time_new)
-        print(area_l)
-        area_r = integrate.trapz(spl_tremor_r, x = time_new)
-        print(area_r)
+        # Простое скользящее среднее
+        arr_x = table.values[:, 1]
+        arr_y_r = table.values[:, 5]
+        arr_y_l = table.values[:, 9]
+        window_size = 15
+        
+        i = 0
+        # Initialize an empty list to store moving averages
+        moving_averages_x = []
+        moving_averages_y_r = []
+        moving_averages_y_l = []
+        
+        # Loop through the array t o
+        #consider every window of size 3
+        while (i < len(arr_x) - window_size + 1) & (i < len(arr_y_l) - window_size + 1) & (i < len(arr_y_r) - window_size + 1):
+        
+            # Calculate the average of current window
+            window_average_x = round(np.sum(arr_x[i:i+window_size]) / window_size, 2)
+            window_average_y_r = round(np.sum(arr_y_r[i:i+window_size]) / window_size, 2)
+            window_average_y_l = round(np.sum(arr_y_l[i:i+window_size]) / window_size, 2)
+
+            # Store the average of current
+            # window in moving average list
+            moving_averages_x.append(window_average_x)
+            moving_averages_y_r.append(window_average_y_r)
+            moving_averages_y_l.append(window_average_y_l)
+
+            # Shift window to right by one position
+            i += 1
+
+        
+        # Convert array of integers to pandas series
+        numbers_series_x2 = pd.Series(arr_x)
+        numbers_series_y_r2 = pd.Series(arr_y_r)
+        numbers_series_y_l2 = pd.Series(arr_y_r)
+        
+        # Get the moving averages of series
+        # of observations till the current time
+        moving_averages_x2 = round(numbers_series_x2.ewm(alpha=0.1, adjust=False).mean(), 2)
+        moving_averages_y_r2 = round(numbers_series_y_r2.ewm(alpha=0.1, adjust=False).mean(), 2)
+        moving_averages_y_l2 = round(numbers_series_y_l2.ewm(alpha=0.1, adjust=False).mean(), 2)
+
+        # Convert pandas series back to list
+        moving_averages_list_x2 = moving_averages_x2.tolist()
+        moving_averages_list_y_r2 = moving_averages_y_r2.tolist()
+        moving_averages_list_y_l2 = moving_averages_y_l2.tolist()
+
 
     # отрисовка данных
-        # plot l_tremor
-        plt.subplot(2, 2, 1)
+        # plot l_spectrum
+        plt.subplot(2, 4, 1)
         
         # initial date
-        plt.plot(time, l_tremor, color='r')
+        plt.plot(hz, l_spectrum, color='r')
+        plt.grid()
+        plt.ylabel("l_spectrum")
+        plt.xlabel("Исходные данные")
 
         # interpolation
-        #plt.plot(time_new, l_tremor_new (time_new), '--',  color='b')
+        #plt.plot(hz_new, l_spectrum_new (hz_new), '--',  color='b')
 
         # splain
-        plt.plot(time_new, spl_tremor_l, '--',  color='b')
+        plt.subplot(2, 4, 2)
+        plt.plot(hz_new, spl_spectrum_l, '--',  color='b')
         plt.grid()
-        plt.ylim(-130, 130)
-        plt.xlabel("time")
-        plt.ylabel("l_tremor")
-        plt.legend(['date', 'splain'])
+        plt.xlabel("Кубический сплайн")
+
+        # skolz
+        plt.subplot(2, 4, 3)
+        plt.plot(moving_averages_x, moving_averages_y_r, '--',  color='b')
+        plt.grid()
+        plt.xlabel("Простое скользящее среднее, n = 15")
+
+        # eps
+        plt.subplot(2, 4, 4)
+        plt.plot(moving_averages_x2, moving_averages_y_r2, '--',  color='b')
+        plt.grid()
+        plt.xlabel("Экспон. скользящее среднее, альфа = 0.1")
 
         # plot r_tremor
         #
         #
-        plt.subplot(2, 2, 3)
+        plt.subplot(2, 4, 5)
 
         # initial date
-        plt.plot(time, r_tremor, color='r')
+        plt.plot(hz, r_spectrum, color='r')
+        plt.grid()
+        plt.ylabel("r_spectrum")
+        plt.xlabel("Исходные данные")
 
         # interpolation
         #plt.plot(time_new, r_tremor_new (time_new), '--',  color='b')
 
         # splain
-        plt.plot(time_new, spl_tremor_r, '--',  color='b')
+        plt.subplot(2, 4, 6)
+        plt.plot(hz_new, spl_spectrum_r, '--',  color='b')
         plt.grid()
-        plt.ylim(-130, 130)
-        plt.xlabel("time")
-        plt.ylabel("r_tremor")
-        plt.legend(['data', 'splain'])
+        plt.xlabel("Кубический сплайн")
 
-        # шаблон для левой руки
-        plt.subplot(2, 2, 2)
-        plt.plot(time, l_tremor, color='r')
+        plt.subplot(2, 4, 7)
+        plt.plot(moving_averages_x, moving_averages_y_l, '--',  color='b')
         plt.grid()
-        plt.ylim(-130, 130)
-        plt.xlabel("time")
-        plt.ylabel("l_tremor")
+        plt.xlabel("Простое скользящее среднее, n = 15")
 
-        # шаблон данных для правой руки
-        plt.subplot(2, 2, 4)
-        plt.plot(time, r_tremor, color='r')
+        plt.subplot(2, 4, 8)
+        plt.plot(moving_averages_x2, moving_averages_y_l2, '--',  color='b')
         plt.grid()
-        plt.ylim(-130, 130)
-        plt.xlabel("time")
-        plt.ylabel("r_tremor")
+        plt.xlabel("Экспон. скользящее среднее, альфа = 0.1")
 
         plt.show()
     
