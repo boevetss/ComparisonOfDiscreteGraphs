dde9e186bd301d1144fb198e875e01bbfc7fe67d
boevetss <73750523+boevetss@users.noreply.github.com>
2022-10-30 18:13:42 +0300

    перенесла изменения

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from scipy import interpolate
 from scipy import integrate
 import numpy as np
 from tkinter import *
+from sklearn import metrics
 
 def main ():
+    # выделение диапазонов
+        def _palki(step, graph, color):
+            ax.axvspan(step - 2, step, color = color, alpha = 0.4)
+            
+        def mnogo_palok(ax):
+            _palki(2, ax, "red")
+            _palki(4, ax, "orange")
+            _palki(6, ax, "yellow")
+            _palki(8, ax, "yellow")
+            _palki(10, ax, "green")
+            _palki(12, ax, "#4DA6FF")
+            _palki(14, ax, "blue")
+
     # обработка данных
         # для примера используется файл с измерениями P1_2 
         table = pd.read_excel('examlpe.xlsx')
@@ -19,7 +33,7 @@ def main ():
         r_spectrum = table.values[:, 9]
 
         # одномерный массив
-        hz_new = np.linspace(hz.min(), hz.max(), 200)
+        hz_new = np.linspace(hz.min(), hz.max(), int(len(hz) / 4))
 
         # k = 3 - кубический сплайн
         spl_l = interpolate.make_interp_spline(hz, l_spectrum, k=3)
@@ -74,69 +88,85 @@ def main ():
         moving_averages_list_y_r2 = moving_averages_y_r2.tolist()
         moving_averages_list_y_l2 = moving_averages_y_l2.tolist()
 
-
     # отрисовка данных
         # plot l_spectrum
+
+        # init date
         plt.subplot(2, 4, 1)
-        
-        # initial date
         plt.plot(hz, l_spectrum, color='r')
         plt.grid()
         plt.ylabel("l_spectrum")
         plt.xlabel("Исходные данные")
 
-        # interpolation
-        #plt.plot(hz_new, l_spectrum_new (hz_new), '--',  color='b')
-
         # splain
-        plt.subplot(2, 4, 2)
+        ax = plt.subplot(2, 4, 2)
+        #ymin, ymax = plt. ylim()
         plt.plot(hz_new, spl_spectrum_l, '--',  color='b')
+        mnogo_palok(ax)
         plt.grid()
         plt.xlabel("Кубический сплайн")
 
         # skolz
-        plt.subplot(2, 4, 3)
+        ax = plt.subplot(2, 4, 3)
         plt.plot(moving_averages_x, moving_averages_y_r, '--',  color='b')
+        mnogo_palok(ax)
         plt.grid()
         plt.xlabel("Простое скользящее среднее, n = 15")
 
         # eps
-        plt.subplot(2, 4, 4)
+        ax = plt.subplot(2, 4, 4)       
         plt.plot(moving_averages_x2, moving_averages_y_r2, '--',  color='b')
+        mnogo_palok(ax)
         plt.grid()
         plt.xlabel("Экспон. скользящее среднее, альфа = 0.1")
 
         # plot r_tremor
-        #
-        #
-        plt.subplot(2, 4, 5)
+        
 
         # initial date
+        plt.subplot(2, 4, 5)
         plt.plot(hz, r_spectrum, color='r')
         plt.grid()
         plt.ylabel("r_spectrum")
         plt.xlabel("Исходные данные")
 
-        # interpolation
-        #plt.plot(time_new, r_tremor_new (time_new), '--',  color='b')
-
         # splain
-        plt.subplot(2, 4, 6)
+        ax = plt.subplot(2, 4, 6)
+        mnogo_palok(ax)
         plt.plot(hz_new, spl_spectrum_r, '--',  color='b')
         plt.grid()
         plt.xlabel("Кубический сплайн")
 
-        plt.subplot(2, 4, 7)
+        # skolz
+        ax = plt.subplot(2, 4, 7)
+        mnogo_palok(ax)
         plt.plot(moving_averages_x, moving_averages_y_l, '--',  color='b')
         plt.grid()
         plt.xlabel("Простое скользящее среднее, n = 15")
 
-        plt.subplot(2, 4, 8)
+        # eps
+        ax = plt.subplot(2, 4, 8)
+        mnogo_palok(ax)
         plt.plot(moving_averages_x2, moving_averages_y_l2, '--',  color='b')
         plt.grid()
         plt.xlabel("Экспон. скользящее среднее, альфа = 0.1")
 
         plt.show()
+
+        error_hz = []
+        for el in hz_new:
+            error_hz.extend([el, el, el, el])
+
+
+    # оценка точности
+        # среднняя абсолютная ошибка
+        print(f'error = {metrics.mean_absolute_error(error_hz, hz)}')
+
+        # среднеквадратическая ошибка MSE
+        print(f'error = {metrics.mean_squared_error(error_hz, hz)}')
+
+        # среднеквадратическая ошибка RMSE
+        print(f'error = {np.sqrt(metrics.mean_squared_error(error_hz, hz))}')
     
 if __name__ == "__main__":
 	main()
